c56453da51fbb164f3af0dfce2ec28c54b443585
HadyRaed <hraed666@gmail.com>
2020-10-27 12:42:41 +0200

    fetch handling errors

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,7 +1,10 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle} from 'reactstrap';
+
+import { Card, CardImg, CardImgOverlay, Breadcrumb, BreadcrumbItem,CardBody,CardSubtitle,CardText,
+    CardTitle } from 'reactstrap';
 import {Loading} from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
 function RenderCard({item,isLoading,errMess}) {
 
     if (isLoading)
@@ -20,16 +23,14 @@ function RenderCard({item,isLoading,errMess}) {
 }
 else
 
-    return(
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-            <CardTitle>{item.name}</CardTitle>
-            {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
-            <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    );
+     return (item?<Card>
+                <CardBody>
+                <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardTitle>{item.name}</CardTitle>
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle>: null}
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>:null);
 
 }
 
@@ -42,7 +43,9 @@ function Home(props) {
                     errMess= {props.dishesErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={props.promotion} />
+    
+
+                    <RenderCard item={props.promotion} isLoading={props.promoLoading} errMess={props.promoErrMess} />
                 </div>
                 <div className="col-12 col-md m-1">
                     <RenderCard item={props.leader} />
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,6 +1,6 @@
 import { DISHES } from '../shared/dishes';
 import * as ActionTypes from './ActionTypes';
-
+import { baseUrl } from '../shared/baseUrl';
 export const addComment = (dishId, rating, author, comment) => ({
     type: ActionTypes.ADD_COMMENT,
     payload: {
@@ -11,15 +11,95 @@ export const addComment = (dishId, rating, author, comment) => ({
     }
 });
 
-export const fetchDishes = () =>
-(dispatch) => {
-dispatch(dishesLoading(true));
+export const fetchDishes = () => (dispatch) => {
 
-setTimeout (() => {dispatch(addDishes(DISHES))},2000);
+    dispatch(dishesLoading(true));
 
-
-
+    return fetch(baseUrl + 'dishes')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(dishes => dispatch(addDishes(dishes)))
+    .catch(error => dispatch(dishesFailed(error.message)));
 }
+
+export const fetchComments = () => (dispatch) => {    
+    return fetch(baseUrl + 'comments')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(comments => dispatch(addComments(comments)))
+    .catch(error => dispatch(commentsFailed(error.message)));
+};
+
+export const commentsFailed = (errmess) => ({
+    type: ActionTypes.COMMENTS_FAILED,
+    payload: errmess
+});
+
+export const addComments = (comments) => ({
+    type: ActionTypes.ADD_COMMENTS,
+    payload: comments
+});
+
+export const fetchPromos = () => (dispatch) => {
+    
+    dispatch(promosLoading());
+
+    return fetch(baseUrl + 'promotions')
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            var errmess = new Error(error.message);
+            throw errmess;
+      })
+    .then(response => response.json())
+    .then(promos => dispatch(addPromos(promos)))
+    .catch(error => dispatch(promosFailed(error.message)));
+}
+
+export const promosLoading = () => ({
+    type: ActionTypes.PROMOS_LOADING
+});
+
+export const promosFailed = (errmess) => ({
+    type: ActionTypes.PROMOS_FAILED,
+    payload: errmess
+});
+
+export const addPromos = (promos) => ({
+    type: ActionTypes.ADD_PROMOS,
+    payload: promos
+});
 
 export const dishesLoading = () => ({
 
